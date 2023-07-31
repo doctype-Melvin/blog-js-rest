23e9a81b198e50cba53e3d97f94133bbc7bd2866
doctype-Melvin <doctype.melvin@gmail.com>
2023-07-31 12:01:16 +0200

    add intro to home page - header is home link

--- secret-sauce-blog/app/layout.js
+++ secret-sauce-blog/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import localFont from '@next/font/local';
+import Link from 'next/link';
 
 const roboto = localFont({
   src: [
@@ -28,15 +29,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en' className={`${roboto.variable} font-sans`}>
       <body>
-        <h1 className='text-center text-3xl'>🍯 | Honeypot Blog | 🍯</h1>
-        <p>
-          Welcome to the Honeypot Blog, where sweetness awaits at every turn!
-          Dive into our hive of articles, written with love and buzzing with
-          creativity. And the best part? You&apos;re not alone in this
-          indulgence. Share the sweetness with your friends by forwarding your
-          favorite posts and spreading the joy of discovery. The adventure
-          awaits! 🍯📚🐝
-        </p>
+        <Link href={`/`} className='text-center text-3xl'>
+          🍯 | Honeypot Blog | 🍯
+        </Link>
+
         {children}
       </body>
     </html>
--- secret-sauce-blog/app/page.js
+++ secret-sauce-blog/app/page.js
@@ -16,6 +16,13 @@ export default async function Home() {
   const allPosts = await getAllPosts();
   return (
     <main>
+      <p>
+        Welcome to the Honeypot Blog, where sweetness awaits at every turn! Dive
+        into our hive of articles, written with love and buzzing with
+        creativity. And the best part? You&apos;re not alone in this indulgence.
+        Share the sweetness with your friends by forwarding your favorite posts
+        and spreading the joy of discovery. The adventure awaits! 🍯📚🐝
+      </p>
       <ul aria-label='grid of post previews'>
         {allPosts.map((post) => (
           <PostPreview key={post.id} data={post} />
