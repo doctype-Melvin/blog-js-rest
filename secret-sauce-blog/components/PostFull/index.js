b59c9b33f48f92dc3599d2c208231b603ebda086
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-01 07:56:13 +0200

    add styling to detail page

--- secret-sauce-blog/components/PostFull/index.js
+++ secret-sauce-blog/components/PostFull/index.js
@@ -6,22 +6,27 @@ export default function PostFull({ data }) {
   const datePublished = format(new Date(data.created), 'MM/dd/yyyy');
 
   return (
-    <section aria-label='full post'>
-      <section aria-label='post header'>
+    <section aria-label='full post' className='w-11/12 md:w-3/4 lg:w-3/5'>
+      <section aria-label='post header' className='flex w-full justify-between'>
         <p aria-label='author'>{data.author}</p>
         <p aria-label='date'>{datePublished}</p>
       </section>
       <Share data={data} />
       <section aria-label='post content'>
-        <h1 aria-label='post title'>{data.title}</h1>
-        <h2 aria-label='post subtitle'>{data.subtitle}</h2>
+        <h1 aria-label='post title' className='text-3xl'>
+          {data.title}
+        </h1>
+        <h2 aria-label='post subtitle' className='text-2xl'>
+          {data.subtitle}
+        </h2>
         <Image
           src={data.image}
           alt={'beautiful image'}
           width={250}
           height={250}
+          className='mx-auto'
         />
-        <p>{data.content}</p>
+        <p className='text-justify'>{data.content}</p>
         <Share data={data} />
       </section>
       {/* Place share button here */}
