d65157daf16b744f677219d2b6ae8e3f868c7054
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-02 10:26:19 +0200

    add loading spinner

--- secret-sauce-blog/app/loading.js
+++ secret-sauce-blog/app/loading.js
@@ -1,3 +1,11 @@
 export default function Loading() {
-  return <div className='flex-1'>Loading content! Hang in there!</div>;
+  return (
+    <div className='flex flex-1 items-center justify-center'>
+      <div
+        className='inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+        role='status'
+      ></div>
+      <div className='ml-3 text-xl'>Loading content</div>
+    </div>
+  );
 }
