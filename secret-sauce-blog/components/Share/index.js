7a83bdc60d3843b033d4cb2db826fad9677907c9
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-02 08:07:04 +0200

    add copy message modal

--- secret-sauce-blog/components/Share/index.js
+++ secret-sauce-blog/components/Share/index.js
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   EmailShareButton,
   RedditShareButton,
@@ -11,19 +12,26 @@ import {
 } from 'react-icons/ai';
 import { FiTwitter as Twitter, FiFacebook as FB } from 'react-icons/fi';
 import { BsShare as CopyShare } from 'react-icons/bs';
+import { useState, useEffect } from 'react';
 
 export default function Share({ data }) {
   const shareUrl = `http://localhost:3000/posts/${data.id}`;
+  const [copyLink, setCopyLink] = useState(false);
 
   const handleCopy = async (url) => {
     await navigator.clipboard.writeText(url);
     // Set Timeout for clipboard modal message
+    setCopyLink(true);
   };
+
+  useEffect(() => {
+    setTimeout(() => setCopyLink(false), 1250);
+  }, [copyLink]);
 
   return (
     <section
       // Set with for all other screen sizes
-      className='flex justify-around py-3 sm:w-1/5 md:w-2/3'
+      className='relative flex justify-around py-3 sm:w-1/5 md:w-2/3'
       aria-label='Social Share Buttons'
     >
       <EmailShareButton
@@ -41,6 +49,11 @@ export default function Share({ data }) {
       <FacebookShareButton url={shareUrl} quote={data.title}>
         <FB className='h-6 w-6 cursor-pointer' />
       </FacebookShareButton>
+      {copyLink && (
+        <span className='absolute -top-5 right-0 rounded bg-black p-1 text-sm text-white'>
+          Link Copied!
+        </span>
+      )}
       <CopyShare
         onClick={() => handleCopy(shareUrl)}
         className='h-6 w-6 cursor-pointer'
