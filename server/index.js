eb284bc65dcca45346c8b9b0947f3f4d2c0bf806
doctype-Melvin <doctype.melvin@gmail.com>
2023-08-03 08:01:43 +0200

    add env to server

--- server/index.js
+++ server/index.js
@@ -3,35 +3,35 @@ const { ApolloServer } = require("apollo-server-express");
 const { blogposts } = require("./data");
 const { typeDefs } = require("./typeDefs");
 const { resolvers } = require("./resolvers");
-const cors=require("cors");
+const cors = require("cors");
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 
 server.applyMiddleware({ app });
-app.use(cors())
+app.use(cors());
 
 /* REST API */
 app.get("/rest/blogposts", (_request, res) => {
-  res.send(blogposts);
+	res.send(blogposts);
 });
 app.get("/rest/blogpost/:id", (request, res) => {
-  const blogpost = blogposts.find(
-    (blogpost) => blogpost.id === request.params.id
-  );
-  if (!blogpost) {
-    return res.status(404).send();
-  }
+	const blogpost = blogposts.find(
+		(blogpost) => blogpost.id === request.params.id
+	);
+	if (!blogpost) {
+		return res.status(404).send();
+	}
 
-  res.send(blogpost);
+	res.send(blogpost);
 });
 
 app.listen(port, () => {
-  console.log(
-    `🚀 GraphQL Server ready at http://localhost:${port}${server.graphqlPath}`
-  );
-  console.log(`🚀 REST Server ready at http://localhost:${port}/rest`);
+	console.log(
+		`🚀 GraphQL Server ready at http://localhost:${port}${server.graphqlPath}`
+	);
+	console.log(`🚀 REST Server ready at http://localhost:${port}/rest`);
 });
